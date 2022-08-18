f4ea6837f0ead6a8c1cbf21f119f35df757a3401
lbressler13 <lbressler13@gmail.com>
2022-08-17 23:28:57 -0400

    Use gcd functions from kotlin utils (#13)
    refactor: use gcd functions from kotlin utils

--- exact-numbers/src/main/kotlin/common/utils.kt
+++ exact-numbers/src/main/kotlin/common/utils.kt
@@ -1,42 +1,9 @@
 package common
 
-import kotlinutils.biginteger.ext.isZero
 import java.math.BigDecimal
 import java.math.BigInteger
 import java.math.MathContext
 import java.math.RoundingMode
-
-/**
- * Get positive greatest common divisor of 2 numbers using Euclidean algorithm
- */
-internal fun getGCD(val1: BigInteger, val2: BigInteger): BigInteger {
-    val aval1 = val1.abs()
-    val aval2 = val2.abs()
-
-    when {
-        aval1.isZero() && aval2.isZero() -> return BigInteger.ONE
-        aval1.isZero() -> return aval2
-        aval2.isZero() -> return aval1
-        aval1 == aval2 -> return aval1
-    }
-
-    var sum = aval1.max(aval2)
-    var value = aval1.min(aval2)
-    var finished = false
-
-    while (!finished) {
-        val remainder = sum % value
-
-        if (remainder.isZero()) {
-            finished = true
-        } else {
-            sum = value
-            value = remainder
-        }
-    }
-
-    return value
-}
 
 /**
  * Perform division of two BigDecimals, rounding to 20 digits if the result is irrational
