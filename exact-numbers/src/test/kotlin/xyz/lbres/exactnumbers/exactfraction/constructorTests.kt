d245d1c2ba056745fa5d3d4e5954b2335f889bac
lbressler13 <lbressler13@gmail.com>
2023-08-27 19:55:37 -0400

    refactor: multitype helper for ef constructor

--- exact-numbers/src/test/kotlin/xyz/lbres/exactnumbers/exactfraction/constructorTests.kt
+++ exact-numbers/src/test/kotlin/xyz/lbres/exactnumbers/exactfraction/constructorTests.kt
@@ -41,335 +41,106 @@ private fun testStringConstructor() {
 }
 
 private fun testSingleValConstructor() {
-    // BigInteger
-    var ef = ExactFraction(zero)
-    assertEquals(ef.numerator, zero)
-    assertEquals(ef.denominator, pos1)
+    runMultiTypeSingleValTest(0) {
+        assertEquals(it.numerator, zero)
+        assertEquals(it.denominator, pos1)
+    }
 
-    ef = ExactFraction(pos3)
-    assertEquals(ef.numerator, pos3)
-    assertEquals(ef.denominator, pos1)
+    runMultiTypeSingleValTest(3) {
+        assertEquals(it.numerator, pos3)
+        assertEquals(it.denominator, pos1)
+    }
 
-    ef = ExactFraction(neg3)
-    assertEquals(ef.numerator, neg3)
-    assertEquals(ef.denominator, pos1)
+    runMultiTypeSingleValTest(-3) {
+        assertEquals(it.numerator, neg3)
+        assertEquals(it.denominator, pos1)
+    }
+}
 
-    // Int
-    ef = ExactFraction(0)
-    assertEquals(ef.numerator, zero)
-    assertEquals(ef.denominator, pos1)
+private fun testPairValConstructor() {
+    // denominator of 0
+    assertDivByZero { ExactFraction(pos1, zero) }
+    assertDivByZero { ExactFraction(1, 0) }
+    assertDivByZero { ExactFraction(1L, 0L) }
+    assertDivByZero { ExactFraction(pos1, 0) }
+    assertDivByZero { ExactFraction(1, zero) }
+    assertDivByZero { ExactFraction(1, 0L) }
+    assertDivByZero { ExactFraction(1L, 0) }
+    assertDivByZero { ExactFraction(pos1, 0L) }
+    assertDivByZero { ExactFraction(1L, zero) }
 
-    ef = ExactFraction(3)
-    assertEquals(ef.numerator, pos3)
-    assertEquals(ef.denominator, pos1)
+    // numerator of 0
+    runMultiTypePairValTest(0, 1) {
+        assertEquals(zero, it.numerator)
+        assertEquals(pos1, it.denominator)
+    }
 
-    ef = ExactFraction(-3)
-    assertEquals(ef.numerator, neg3)
-    assertEquals(ef.denominator, pos1)
+    // positive whole
+    runMultiTypePairValTest(4, 1) {
+        assertEquals(pos4, it.numerator)
+        assertEquals(pos1, it.denominator)
+    }
 
-    // Long
-    ef = ExactFraction(0L)
-    assertEquals(ef.numerator, zero)
-    assertEquals(ef.denominator, pos1)
+    // positive fraction < 1
+    runMultiTypePairValTest(7, 18) {
+        assertEquals(pos7, it.numerator)
+        assertEquals(pos18, it.denominator)
+    }
 
-    ef = ExactFraction(3L)
-    assertEquals(ef.numerator, pos3)
-    assertEquals(ef.denominator, pos1)
+    // positive fraction > 1
+    runMultiTypePairValTest(4, 3) {
+        assertEquals(pos4, it.numerator)
+        assertEquals(pos3, it.denominator)
+    }
 
-    ef = ExactFraction(-3L)
-    assertEquals(ef.numerator, neg3)
-    assertEquals(ef.denominator, pos1)
+    // negative whole
+    runMultiTypePairValTest(-4, 1) {
+        assertEquals(neg4, it.numerator)
+        assertEquals(pos1, it.denominator)
+    }
+
+    // negative fraction > -1
+    runMultiTypePairValTest(-1, 3) {
+        assertEquals(neg1, it.numerator)
+        assertEquals(pos3, it.denominator)
+    }
+
+    // negative fraction < -1
+    runMultiTypePairValTest(-7, 4) {
+        assertEquals(neg7, it.numerator)
+        assertEquals(pos4, it.denominator)
+    }
 }
 
 /**
+ * Run test with Int, Long, and BigInteger values
  *
- * Cases for each pair:
- * Numerator of 0
- * Denominator of 0 (throws error)
- * Positive whole
- * Positive < 1
- * Positive > 1
- * Negative whole
- * Negative > -1
- * Negative < -1
+ * @param value [Int]: value to cast to Int, Long, and BigInteger
+ * @param validateResult ([ExactFraction]) -> Unit: function to validate constructed fraction
  */
-private fun testPairValConstructor() {
+private fun runMultiTypeSingleValTest(value: Int, validateResult: (ExactFraction) -> Unit) {
+    validateResult(ExactFraction(value))
+    validateResult(ExactFraction(value.toLong()))
+    validateResult(ExactFraction(value.toBigInteger()))
+}
+
+private fun runMultiTypePairValTest(value1: Int, value2: Int, validateResult: (ExactFraction) -> Unit) {
     // BigInteger, BigInteger
-    var ef = ExactFraction(zero, pos1)
-    assertEquals(zero, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    assertDivByZero { ExactFraction(pos1, zero) }
-
-    ef = ExactFraction(pos4, pos1)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(pos7, pos18)
-    assertEquals(pos7, ef.numerator)
-    assertEquals(pos18, ef.denominator)
-
-    ef = ExactFraction(pos4, pos3)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(neg4, pos1)
-    assertEquals(neg4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(neg1, pos3)
-    assertEquals(neg1, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(neg7, pos4)
-    assertEquals(neg7, ef.numerator)
-    assertEquals(pos4, ef.denominator)
-
+    validateResult(ExactFraction(value1.toBigInteger(), value2.toBigInteger()))
     // Int, Int
-    ef = ExactFraction(0, 1)
-    assertEquals(zero, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    assertDivByZero { ExactFraction(1, 0) }
-
-    ef = ExactFraction(4, 1)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(7, 18)
-    assertEquals(pos7, ef.numerator)
-    assertEquals(pos18, ef.denominator)
-
-    ef = ExactFraction(4, 3)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(-4, 1)
-    assertEquals(neg4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(-1, 3)
-    assertEquals(neg1, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(-7, 4)
-    assertEquals(neg7, ef.numerator)
-    assertEquals(pos4, ef.denominator)
-
+    validateResult(ExactFraction(value1, value2))
     // Long, Long
-    ef = ExactFraction(0L, 1L)
-    assertEquals(zero, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    assertDivByZero { ExactFraction(1L, 0L) }
-
-    ef = ExactFraction(4L, 1L)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(7L, 18L)
-    assertEquals(pos7, ef.numerator)
-    assertEquals(pos18, ef.denominator)
-
-    ef = ExactFraction(4L, 3L)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(-4L, 1L)
-    assertEquals(neg4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(-1L, 3L)
-    assertEquals(neg1, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(-7L, 4L)
-    assertEquals(neg7, ef.numerator)
-    assertEquals(pos4, ef.denominator)
-
-    // BigInteger, Int
-    ef = ExactFraction(zero, 1)
-    assertEquals(zero, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    assertDivByZero { ExactFraction(pos1, 0) }
-
-    ef = ExactFraction(pos4, 1)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(pos7, 18)
-    assertEquals(pos7, ef.numerator)
-    assertEquals(pos18, ef.denominator)
-
-    ef = ExactFraction(pos4, 3)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(neg4, 1)
-    assertEquals(neg4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(neg1, 3)
-    assertEquals(neg1, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(neg7, 4)
-    assertEquals(neg7, ef.numerator)
-    assertEquals(pos4, ef.denominator)
-
-    // Int, BigInteger
-    ef = ExactFraction(0, pos1)
-    assertEquals(zero, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    assertDivByZero { ExactFraction(1, zero) }
-
-    ef = ExactFraction(4, pos1)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(7, pos18)
-    assertEquals(pos7, ef.numerator)
-    assertEquals(pos18, ef.denominator)
-
-    ef = ExactFraction(4, pos3)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(-4, pos1)
-    assertEquals(neg4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(-1, pos3)
-    assertEquals(neg1, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(-7, pos4)
-    assertEquals(neg7, ef.numerator)
-    assertEquals(pos4, ef.denominator)
-
+    validateResult(ExactFraction(value1.toLong(), value2.toLong()))
     // Int, Long
-    ef = ExactFraction(0, 1L)
-    assertEquals(zero, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    assertDivByZero { ExactFraction(1, 0L) }
-
-    ef = ExactFraction(4, 1L)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(7, 18L)
-    assertEquals(pos7, ef.numerator)
-    assertEquals(pos18, ef.denominator)
-
-    ef = ExactFraction(4, 3L)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(-4, 1L)
-    assertEquals(neg4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(-1, 3L)
-    assertEquals(neg1, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(-7, 4L)
-    assertEquals(neg7, ef.numerator)
-    assertEquals(pos4, ef.denominator)
-
+    validateResult(ExactFraction(value1, value2.toLong()))
     // Long, Int
-    ef = ExactFraction(0L, 1)
-    assertEquals(zero, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    assertDivByZero { ExactFraction(1L, 0) }
-
-    ef = ExactFraction(4L, 1)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(7L, 18)
-    assertEquals(pos7, ef.numerator)
-    assertEquals(pos18, ef.denominator)
-
-    ef = ExactFraction(4L, 3)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(-4L, 1)
-    assertEquals(neg4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(-1L, 3)
-    assertEquals(neg1, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(-7L, 4)
-    assertEquals(neg7, ef.numerator)
-    assertEquals(pos4, ef.denominator)
-
+    validateResult(ExactFraction(value1.toLong(), value2))
+    // BigInteger, Int
+    validateResult(ExactFraction(value1.toBigInteger(), value2))
+    // Int, BigInteger
+    validateResult(ExactFraction(value1, value2.toBigInteger()))
     // BigInteger, Long
-    ef = ExactFraction(zero, 1L)
-    assertEquals(zero, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    assertDivByZero { ExactFraction(pos1, 0L) }
-
-    ef = ExactFraction(pos4, 1L)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(pos7, 18L)
-    assertEquals(pos7, ef.numerator)
-    assertEquals(pos18, ef.denominator)
-
-    ef = ExactFraction(pos4, 3L)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(neg4, 1L)
-    assertEquals(neg4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(neg1, 3L)
-    assertEquals(neg1, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(neg7, 4L)
-    assertEquals(neg7, ef.numerator)
-    assertEquals(pos4, ef.denominator)
-
+    validateResult(ExactFraction(value1.toBigInteger(), value2.toLong()))
     // Long, BigInteger
-    ef = ExactFraction(0L, pos1)
-    assertEquals(zero, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    assertDivByZero { ExactFraction(1L, zero) }
-
-    ef = ExactFraction(4L, pos1)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(7L, pos18)
-    assertEquals(pos7, ef.numerator)
-    assertEquals(pos18, ef.denominator)
-
-    ef = ExactFraction(4L, pos3)
-    assertEquals(pos4, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(-4L, pos1)
-    assertEquals(neg4, ef.numerator)
-    assertEquals(pos1, ef.denominator)
-
-    ef = ExactFraction(-1L, pos3)
-    assertEquals(neg1, ef.numerator)
-    assertEquals(pos3, ef.denominator)
-
-    ef = ExactFraction(-7L, pos4)
-    assertEquals(neg7, ef.numerator)
-    assertEquals(pos4, ef.denominator)
+    validateResult(ExactFraction(value1.toLong(), value2.toBigInteger()))
 }
