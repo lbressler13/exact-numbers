e75774d576af78ebb86cf2f4427b0c62da3bbfa7
lbressler13 <lbressler13@gmail.com>
2023-08-28 01:47:35 -0400

    chore: bump import version

--- exact-numbers/build.gradle.kts
+++ exact-numbers/build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 }
 
 dependencies {
-    val kotlinUtilsVersion = "0.2.0"
+    val kotlinUtilsVersion = "1.1.0"
     val mockkVersion = "1.12.4"
 
     implementation(kotlin("stdlib"))
