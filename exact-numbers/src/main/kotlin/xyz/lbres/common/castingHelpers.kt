6597ba494f77c1e1bfafd414b9e2540d09677200
lbressler13 <lbressler13@gmail.com>
2023-12-25 01:20:25 -0500

    refactor: single helper for casting whole and decimal types

--- exact-numbers/src/main/kotlin/xyz/lbres/common/castingHelpers.kt
+++ exact-numbers/src/main/kotlin/xyz/lbres/common/castingHelpers.kt
@@ -3,61 +3,51 @@ package xyz.lbres.common
 import java.math.BigDecimal
 import java.math.BigInteger
 
-internal fun <S : Number> castToWholeNumber(value: BigInteger, minValue: String, maxValue: String, cast: (BigInteger) -> S, getCastingError: (BigDecimal) -> Exception): S {
-    val minInt = BigInteger(minValue)
-    val maxInt = BigInteger(maxValue)
-    if (value in minInt..maxInt) {
-        return cast(value)
-    }
-
-    throw getCastingError(value.toBigDecimal())
-}
-
-internal fun <S : Number> castToDecimal(value: BigDecimal, minValue: S, maxValue: S, cast: (BigDecimal) -> S, getCastingError: (BigDecimal) -> Exception): S {
-    val minDecimal = BigDecimal(minValue.toString())
-    val maxDecimal = BigDecimal(maxValue.toString())
-    if (value in minDecimal..maxDecimal) {
-        return cast(value)
-    }
-
-    throw getCastingError(value)
-}
-
 internal fun castNumberToByte(value: BigDecimal, getCastingError: (BigDecimal) -> Exception): Byte {
-    val minValue = Byte.MIN_VALUE.toString()
-    val maxValue = Byte.MAX_VALUE.toString()
-    return castToWholeNumber(value.toBigInteger(), minValue, maxValue, BigInteger::toByte, getCastingError)
+    return castNumber(value, Byte.MIN_VALUE, Byte.MAX_VALUE, BigDecimal::toByte, getCastingError, false)
 }
 
 internal fun castNumberToChar(value: BigDecimal, getCastingError: (BigDecimal) -> Exception): Char {
-    val minValue = Char.MIN_VALUE.code.toString()
-    val maxValue = Char.MAX_VALUE.code.toString()
-    val code = castToWholeNumber(value.toBigInteger(), minValue, maxValue, BigInteger::toInt, getCastingError)
+    val minValue = Char.MIN_VALUE.code
+    val maxValue = Char.MAX_VALUE.code
+    val code = castNumber(value, minValue, maxValue, BigDecimal::toInt, getCastingError, false)
     return code.toChar()
 }
 
 internal fun castNumberToShort(value: BigDecimal, getCastingError: (BigDecimal) -> Exception): Short {
-    val minValue = Short.MIN_VALUE.toString()
-    val maxValue = Short.MAX_VALUE.toString()
-    return castToWholeNumber(value.toBigInteger(), minValue, maxValue, BigInteger::toShort, getCastingError)
+    return castNumber(value, Short.MIN_VALUE, Short.MAX_VALUE, BigDecimal::toShort, getCastingError, false)
 }
 
 internal fun castNumberToInt(value: BigDecimal, getCastingError: (BigDecimal) -> Exception): Int {
-    val minValue = Int.MIN_VALUE.toString()
-    val maxValue = Int.MAX_VALUE.toString()
-    return castToWholeNumber(value.toBigInteger(), minValue, maxValue, BigInteger::toInt, getCastingError)
+    return castNumber(value, Int.MIN_VALUE, Int.MAX_VALUE, BigDecimal::toInt, getCastingError, false)
 }
 
 internal fun castNumberToLong(value: BigDecimal, getCastingError: (BigDecimal) -> Exception): Long {
-    val minValue = Long.MIN_VALUE.toString()
-    val maxValue = Long.MAX_VALUE.toString()
-    return castToWholeNumber(value.toBigInteger(), minValue, maxValue, BigInteger::toLong, getCastingError)
+    return castNumber(value, Long.MIN_VALUE, Long.MAX_VALUE, BigDecimal::toLong, getCastingError, false)
 }
 
 internal fun castNumberToFloat(value: BigDecimal, getCastingError: (BigDecimal) -> Exception): Float {
-    return castToDecimal(value, -Float.MAX_VALUE, Float.MAX_VALUE, BigDecimal::toFloat, getCastingError)
+    return castNumber(value, -Float.MAX_VALUE, Float.MAX_VALUE, BigDecimal::toFloat, getCastingError, true)
 }
 
 internal fun castNumberToDouble(value: BigDecimal, getCastingError: (BigDecimal) -> Exception): Double {
-    return castToDecimal(value, -Double.MAX_VALUE, Double.MAX_VALUE, BigDecimal::toDouble, getCastingError)
+    return castNumber(value, -Double.MAX_VALUE, Double.MAX_VALUE, BigDecimal::toDouble, getCastingError, true)
+}
+
+private fun <T : Number> castNumber(value: BigDecimal, minValue: T, maxValue: T, cast: (BigDecimal) -> T, getCastingError: (BigDecimal) -> Exception, isDecimal: Boolean): T {
+    val minString = minValue.toString()
+    val maxString = maxValue.toString()
+
+    val validValue = if (isDecimal) {
+        value in BigDecimal(minString)..BigDecimal(maxString)
+    } else {
+        val intValue = value.toBigInteger()
+        intValue in BigInteger(minString)..BigInteger(maxString)
+    }
+
+    if (validValue) {
+        return cast(value)
+    }
+
+    throw getCastingError(value)
 }
