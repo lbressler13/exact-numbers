8c32e05096b8ca4f6a4ffa324631442d0a0889f4
lbressler13 <lbressler13@gmail.com>
2024-01-07 21:33:50 -0500

    chore: ktlint

--- exact-numbers/src/test/kotlin/xyz/lbres/exactnumbers/expressions/term/compareToTests.kt
+++ exact-numbers/src/test/kotlin/xyz/lbres/exactnumbers/expressions/term/compareToTests.kt
@@ -1,8 +1,4 @@
 package xyz.lbres.exactnumbers.expressions.term
 
-import xyz.lbres.exactnumbers.exactfraction.ExactFraction
-import xyz.lbres.exactnumbers.irrationals.sqrt.Sqrt
-import kotlin.test.assertEquals
-
 fun runCompareToTests() {
 }
