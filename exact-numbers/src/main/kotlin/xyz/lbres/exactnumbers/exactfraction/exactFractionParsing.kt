6e82884fa44cec91e9ce04ca5b3864822a08f73e
lbressler13 <lbressler13@gmail.com>
2024-01-30 23:03:52 -0500

    feat: update ef parsing helpers

--- exact-numbers/src/main/kotlin/xyz/lbres/exactnumbers/exactfraction/exactFractionParsing.kt
+++ exact-numbers/src/main/kotlin/xyz/lbres/exactnumbers/exactfraction/exactFractionParsing.kt
@@ -1,65 +1,65 @@
 package xyz.lbres.exactnumbers.exactfraction
 
-import xyz.lbres.kotlinutils.biginteger.ext.isZero
-import xyz.lbres.kotlinutils.string.ext.substringTo
+// TODO
+// import xyz.lbres.kotlinutils.general.simpleIf
+// import xyz.lbres.kotlinutils.general.succeeds
+// import xyz.lbres.kotlinutils.general.tryOrDefault
+import java.math.BigDecimal
 import java.math.BigInteger
+
+private const val efPrefix = "EF["
+private const val efSuffix = "]"
 
 /**
  * Parse a string from standard number format into a ExactFraction.
  * Standard format is a string which may start with "-", but otherwise consists of at least one digit and up to 1 "."
  *
- * @param unparsed [String]: string to parse
- * @return parsed ExactFraction
- * @throws NumberFormatException in case of improperly formatted number string
+ * @param s [String]: string to parse
+ * @return [ExactFraction]: parsed value
  */
-internal fun parseDecimal(unparsed: String): ExactFraction {
-    var currentState: String = unparsed.trim()
+internal fun parseDecimal(s: String): ExactFraction {
+    var currentState: String = s.trim()
 
-    val isNegative = unparsed.startsWith("-")
-    val timesNeg = if (isNegative) -BigInteger.ONE else BigInteger.ONE
+    // validate string
+    // TODO
+    // if (!succeeds { BigDecimal(currentState) } || currentState.last() == '.') {
+    // throw NumberFormatException("Error parsing $currentState")
+    // }
+    try {
+        BigDecimal(currentState)
+    } catch (_: Exception) {
+        throw NumberFormatException("Error parsing $currentState")
+    }
+
+    if (currentState.last() == '.') {
+        throw NumberFormatException("Error parsing $currentState")
+    }
+
+    // remove negative sign
+    val isNegative = currentState.startsWith("-")
     if (isNegative) {
         currentState = currentState.substring(1)
     }
 
-    val decimalIndex: Int = currentState.indexOf('.')
+    val divResult = BigDecimal(currentState).divideAndRemainder(BigDecimal.ONE)
+    val whole = divResult[0].toBigInteger()
 
-    return when (decimalIndex) {
-        -1 -> {
-            val numerator = BigInteger(currentState)
-            ExactFraction(numerator * timesNeg)
-        }
-        0 -> {
-            currentState = currentState.substring(1)
-            val numerator = BigInteger(currentState)
+    val rawDecimalString = divResult[1].stripTrailingZeros().toPlainString()
+    val decimalIndex = rawDecimalString.indexOf('.')
+    val decimalString = rawDecimalString.substring(decimalIndex + 1) // starts from 0 if decimalIndex == -1
 
-            if (numerator.isZero()) {
-                return ExactFraction(0)
-            }
+    val denomZeroes = "0".repeat(decimalString.length)
+    val denomString = "1$denomZeroes"
 
-            val zeros = "0".repeat(currentState.length)
-            val denomString = "1$zeros"
-            val denominator = BigInteger(denomString)
+    val denominator = BigInteger(denomString)
+    val numerator = whole * denominator + BigInteger(decimalString)
 
-            ExactFraction(numerator * timesNeg, denominator)
-        }
-        else -> {
-            val wholeString = currentState.substringTo(decimalIndex)
-            val decimalString = currentState.substring(decimalIndex + 1)
-            val whole = BigInteger(wholeString)
-            val decimal = BigInteger(decimalString)
-
-            if (decimal.isZero()) {
-                return ExactFraction(whole * timesNeg) // also covers the case where number is 0
-            }
-
-            val zeros = "0".repeat(decimalString.length)
-            val denomString = "1$zeros"
-
-            val denominator = BigInteger(denomString)
-            val numerator = whole * denominator + decimal
-
-            ExactFraction(numerator * timesNeg, denominator)
-        }
+    // TODO
+    // return simpleIf(isNegative, { ExactFraction(-numerator, denominator) }, { ExactFraction(numerator, denominator) })
+    return if (isNegative) {
+        ExactFraction(-numerator, denominator)
+    } else {
+        ExactFraction(numerator, denominator)
     }
 }
 
@@ -67,26 +67,28 @@ internal fun parseDecimal(unparsed: String): ExactFraction {
  * Parse a string from a EF string format into a ExactFraction.
  * EF string format is "EF[num denom]"
  *
- * @param unparsed [String]: string to parse
- * @return parsed ExactFraction
- * @throws NumberFormatException in case of improperly formatted number string
+ * @param s [String]: string to parse
+ * @return [ExactFraction]: parsed value
  */
-internal fun parseEFString(unparsed: String): ExactFraction {
-    if (!checkIsEFString(unparsed)) {
+internal fun parseEFString(s: String): ExactFraction {
+    if (!checkIsEFString(s)) {
+        // TODO
+        // throw NumberFormatException("Invalid EF string format: $s")
         throw NumberFormatException("Invalid EF string format")
     }
 
     try {
-        val numbers = unparsed.substring(3, unparsed.lastIndex)
-        val split = numbers.split(' ')
-        val numString = split[0].trim()
-        val denomString = split[1].trim()
+        val numbers = s.trim().substring(efPrefix.length, s.length - efSuffix.length).split(' ')
+        val numString = numbers[0].trim()
+        val denomString = numbers[1].trim()
         val numerator = BigInteger(numString)
         val denominator = BigInteger(denomString)
         return ExactFraction(numerator, denominator)
     } catch (e: ArithmeticException) {
         throw e
-    } catch (e: Exception) {
+    } catch (_: Exception) {
+        // TODO
+        // throw NumberFormatException("Invalid EF string format: $s")
         throw NumberFormatException("Invalid EF string format")
     }
 }
@@ -96,16 +98,29 @@ internal fun parseEFString(unparsed: String): ExactFraction {
  * EF string format is "EF[num denom]"
  *
  * @param s [String]: string to check
- * @return true if s is in EF string format, false otherwise
+ * @return [Boolean]: `true` if s is in EF string format, `false` otherwise
  */
-fun checkIsEFString(s: String): Boolean {
+internal fun checkIsEFString(s: String): Boolean {
+    val trimmed = s.trim()
+
+    if (!trimmed.startsWith(efPrefix) || !trimmed.endsWith(efSuffix)) {
+        return false
+    }
+
+    // TODO
+    // return tryOrDefault(false) {
     return try {
-        val startEnd = s.trim().startsWith("EF[") && s.trim().endsWith("]")
-        val split = s.substring(3, s.lastIndex).split(" ")
-        BigInteger(split[0])
-        BigInteger(split[1])
-        startEnd && split.size == 2
-    } catch (e: Exception) {
+        val numbers = trimmed.substring(efPrefix.length, s.length - efSuffix.length).split(' ')
+        val validNumber: (String) -> Boolean = {
+            when {
+                it.isEmpty() -> false
+                it.length == 1 -> it[0].isDigit()
+                else -> (it[0] == '-' || it[0].isDigit()) && it.substring(1).all(Char::isDigit)
+            }
+        }
+
+        numbers.size == 2 && validNumber(numbers[0]) && validNumber(numbers[1])
+    } catch (_: Exception) {
         false
     }
 }
