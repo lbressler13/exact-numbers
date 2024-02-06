814cec02f68f86310f7a9147eb2ee0a8e82b9e1e
lbressler13 <lbressler13@gmail.com>
2024-02-06 00:46:21 -0500

    Fix term equality (#43)

--- exact-numbers/build.gradle.kts
+++ exact-numbers/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "xyz.lbres"
-version = "1.0.0"
+version = "1.0.1"
 
 val githubUsername: String? = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
 val githubPassword: String? = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
