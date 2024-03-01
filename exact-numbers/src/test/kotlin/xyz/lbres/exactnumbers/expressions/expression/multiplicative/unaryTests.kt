5abcc37f4711c94fd59a27bff82d6a31f8e52784
lbressler13 <lbressler13@gmail.com>
2024-03-01 03:27:53 -0500

    refactor: separate files for getValue + getSimplified tests

--- exact-numbers/src/test/kotlin/xyz/lbres/exactnumbers/expressions/expression/multiplicative/unaryTests.kt
+++ exact-numbers/src/test/kotlin/xyz/lbres/exactnumbers/expressions/expression/multiplicative/unaryTests.kt
@@ -4,9 +4,7 @@ import xyz.lbres.exactnumbers.exactfraction.ExactFraction
 import xyz.lbres.exactnumbers.expressions.Expression
 import xyz.lbres.exactnumbers.expressions.expression.* // ktlint-disable no-wildcard-imports no-unused-imports
 import xyz.lbres.exactnumbers.expressions.term.Term
-import xyz.lbres.exactnumbers.irrationals.sqrt.Sqrt
 import xyz.lbres.exactnumbers.testutils.assertDivByZero
-import java.math.BigDecimal
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
@@ -69,62 +67,6 @@ fun runInverseTests() {
     val partialInverse = MultiplicativeExpression(simpleExpr1.inverse(), simpleExpr3.inverse())
     expected = MultiplicativeExpression(partialInverse, simpleExpr2.inverse())
     assertEquals(expected, expr.inverse())
-}
-
-fun runGetValueTests() {
-    var expr = MultiplicativeExpression(Expression.ZERO, Expression.ZERO)
-    var expected = BigDecimal.ZERO
-    assertEquals(expected, expr.getValue())
-
-    expr = MultiplicativeExpression(simpleExpr1, simpleExpr1.inverse())
-    expected = BigDecimal.ONE
-    assertEquals(expected, expr.getValue())
-
-    expr = MultiplicativeExpression(simpleExpr1, MultiplicativeExpression(simpleExpr2, simpleExpr2.inverse()))
-    expected = BigDecimal("25.132741228718344")
-    assertEquals(expected, expr.getValue())
-
-    expr = MultiplicativeExpression(simpleExpr2.inverse(), MultiplicativeExpression(simpleExpr2, simpleExpr1))
-    expected = BigDecimal("25.132741228718344")
-    assertEquals(expected, expr.getValue())
-
-    expr = MultiplicativeExpression(partialMultExpr, simpleExpr2)
-    expected = BigDecimal("-58.612243222514359594") // -58.61224322251435719068...
-    assertEquals(expected, expr.getValue())
-
-    expr = MultiplicativeExpression(partialMultExpr.inverse(), -simpleExpr2.inverse())
-    expected = BigDecimal("0.017061281824748112795308108019747092438130381820126559258080078125")
-    // expected = BigDecimal("0.0170612818247481127955653165209070906353536211925663773027158203125")
-    assertEquals(expected, expr.getValue())
-}
-
-fun runGetSimplifiedTests() {
-    var expr = MultiplicativeExpression(Expression.ONE, Expression.ZERO)
-    assertEquals(Expression.ZERO, expr.getSimplified())
-
-    expr = MultiplicativeExpression(partialMultExpr, partialMultExpr.inverse())
-    assertEquals(Expression.ONE, expr.getSimplified())
-    assertEquals(Expression.ONE, expr.getSimplified())
-
-    expr = MultiplicativeExpression(simpleExpr1, Expression.ONE)
-    var term = Term.fromValues(ExactFraction.EIGHT, listOf(pi))
-    assertEquals(SimpleExpression(term), expr.getSimplified())
-
-    expr = MultiplicativeExpression(simpleExpr1, partialMultExpr.inverse())
-    term = Term.fromValues(one, listOf(sqrt1.inverse()))
-    assertEquals(SimpleExpression(term), expr.getSimplified())
-    assertEquals(SimpleExpression(term), expr.getSimplified())
-
-    expr = MultiplicativeExpression(simpleExpr2, simpleExpr1)
-    term = Term.fromValues(ExactFraction(512, 187), listOf(pi, log4))
-    assertEquals(SimpleExpression(term), expr.getSimplified())
-    assertEquals(SimpleExpression(term), expr.getSimplified())
-
-    val expr1 = MultiplicativeExpression(-simpleExpr2, partialMultExpr.inverse())
-    val expr2 = MultiplicativeExpression(simpleExpr1.inverse(), MultiplicativeExpression(simpleExpr3, simpleExpr3))
-    expr = MultiplicativeExpression(expr1, expr2)
-    term = Term.fromValues(ExactFraction(-3, 187), listOf(log4, Sqrt(11), piInverse, piInverse))
-    assertEquals(SimpleExpression(term), expr.getSimplified())
 }
 
 fun runIsZeroTests() {
--- exact-numbers/src/test/kotlin/xyz/lbres/exactnumbers/expressions/expression/simple/unaryTests.kt
+++ exact-numbers/src/test/kotlin/xyz/lbres/exactnumbers/expressions/expression/simple/unaryTests.kt
@@ -6,7 +6,6 @@ import xyz.lbres.exactnumbers.expressions.term.Term
 import xyz.lbres.exactnumbers.irrationals.log.Log
 import xyz.lbres.exactnumbers.irrationals.sqrt.Sqrt
 import xyz.lbres.exactnumbers.testutils.assertDivByZero
-import java.math.BigDecimal
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
@@ -111,90 +110,6 @@ fun runInverseTests() {
     )
     assertEquals(expr1, expr2.inverse())
     assertEquals(expr2, expr1.inverse())
-}
-
-fun runGetValueTests() {
-    var expr = SimpleExpression(Term.ZERO)
-    var expected = BigDecimal.ZERO
-    assertEquals(expected, expr.getValue())
-
-    expr = SimpleExpression(Term.fromValues(ExactFraction.EIGHT, emptyList()))
-    expected = BigDecimal("8")
-    assertEquals(expected, expr.getValue())
-
-    expr = SimpleExpression(Term.fromValues(ExactFraction(-1, 3), emptyList()))
-    expected = BigDecimal("-0.33333333333333333333")
-    assertEquals(expected, expr.getValue())
-
-    expr = SimpleExpression(Term.fromValues(one, listOf(log1, log1.inverse())))
-    expected = BigDecimal.ONE
-    assertEquals(expected, expr.getValue())
-
-    expr = SimpleExpression(Term.fromValues(one, listOf(log1, log2)))
-    expected = BigDecimal("0.61342218956802803344500481172832")
-    assertEquals(expected, expr.getValue())
-
-    expr = SimpleExpression(Term.fromValues(one, listOf(piInverse)))
-    expected = BigDecimal("0.31830988618379069570")
-    assertEquals(expected, expr.getValue())
-
-    expr = SimpleExpression(Term.fromValues(one, listOf(pi, pi, pi)))
-    expected = BigDecimal("31.006276680299813114880451174049119330924860257")
-    assertEquals(expected, expr.getValue())
-
-    expr = SimpleExpression(Term.fromValues(one, listOf(Sqrt(ExactFraction(9, 5)))))
-    expected = BigDecimal("1.34164078649987381784")
-    assertEquals(expected, expr.getValue())
-
-    expr = SimpleExpression(Term.fromValues(one, listOf(Sqrt(121))))
-    expected = BigDecimal("11")
-    assertEquals(expected, expr.getValue())
-
-    expr = SimpleExpression(Term.fromValues(ExactFraction(-8, 3), listOf(pi)))
-    expected = BigDecimal("-8.3775804095727813333")
-    assertEquals(expected, expr.getValue())
-
-    expr = SimpleExpression(Term.fromValues(ExactFraction.FOUR, listOf(Sqrt.ONE, testNumber2)))
-    expected = BigDecimal("28")
-    assertEquals(expected, expr.getValue())
-
-    expr = SimpleExpression(Term.fromValues(ExactFraction.SEVEN, listOf(Log(ExactFraction(6, 7)), Log(40), pi)))
-    expected = BigDecimal("-2.35861167086684457383417423198393663398251286036")
-    assertEquals(expected, expr.getValue())
-
-    expr = SimpleExpression(Term.fromValues(ExactFraction.HALF, listOf(Log(4, 2).inverse(), Log(123456789), piInverse)))
-    expected = BigDecimal("0.64390230285929702583103243749028475")
-    assertEquals(expected, expr.getValue())
-
-    expr = SimpleExpression(Term.fromValues(ExactFraction(3, 11), listOf(Log(5, 2), Sqrt(122), pi)))
-    expected = BigDecimal("21.973899001484265398")
-    assertEquals(expected, expr.getValue())
-
-    expr = SimpleExpression(Term.fromValues(one, listOf(testNumber2, testNumber2, testNumber1, Log(5, 2))))
-    expected = BigDecimal("85.33085748711055350")
-    assertEquals(expected, expr.getValue())
-}
-
-fun runGetSimplifiedTests() {
-    var term = Term.fromValues(ExactFraction.EIGHT, listOf(pi, piInverse))
-    var expected = Term.fromValues(ExactFraction.EIGHT, emptyList())
-    assertEquals(expected, term.getSimplified())
-
-    term = Term.fromValues(ExactFraction(-3, 2), listOf(log1, pi, piInverse, pi))
-    expected = Term.fromValues(ExactFraction(-3, 2), listOf(log1, pi))
-    assertEquals(expected, term.getSimplified())
-
-    term = Term.fromValues(ExactFraction.TWO, listOf(Sqrt(64), Sqrt(ExactFraction(75, 98)), Sqrt(26)))
-    expected = Term.fromValues(ExactFraction(80, 7), listOf(Sqrt(ExactFraction(39))))
-    assertEquals(expected, term.getSimplified())
-
-    term = Term.fromValues(ExactFraction.EIGHT, emptyList())
-    expected = Term.fromValues(ExactFraction.EIGHT, emptyList())
-    assertEquals(expected, term.getSimplified())
-
-    term = Term.fromValues(ExactFraction.SEVEN, listOf(log1, log1, log2.inverse(), Sqrt(5), pi, pi))
-    expected = Term.fromValues(ExactFraction.SEVEN, listOf(log1, log1, log2.inverse(), Sqrt(5), pi, pi))
-    assertEquals(expected, term.getSimplified())
 }
 
 fun runIsZeroTests() {
